85028898c6931914c3f14b75297e78c02b5dcc46
Valentin Phil <Vltnplt@gmail.com>
2025-04-24 14:39:05 +0200

    Ajout start/end & suivi de la ligne

--- carte.js
+++ carte.js
@@ -1,3 +1,8 @@
+/*
+//Philippeville
+let newPosition = {latitude:50.196241, longitude: 4.543508 }
+affStops(map,newPosition, true or false)
+*/
 // Affichage et paramètre de l'icon de ma position
 const iconPosition = L.icon({
     iconUrl: 'icons/icon-map-user-location.svg',
@@ -8,27 +13,29 @@ const iconPosition = L.icon({
 // Affichage et paramètre de l'icon stop
 const iconStop = L.icon({
     iconUrl: 'icons/icon-map-bus-stop.svg',
-    iconSize: [53, 53],
+    iconSize: [45, 45],
     iconAnchor: [16, 55],
     popupAnchor: [10,-37]
 })
 // Affichage et paramètre de l'icon Start
 const iconStart = L.icon({
     iconUrl: 'icons/icon-map-bus-start.svg',
-    iconSize: [53, 53],
+    iconSize: [50, 50],
     iconAnchor: [16, 55],
     popupAnchor: [10,-37]
 })
 // Affichage et paramètre de l'icon End
 const iconEnd = L.icon({
     iconUrl: 'icons/icon-map-bus-end.svg',
-    iconSize: [53, 53],
+    iconSize: [50, 50],
     iconAnchor: [16, 55],
     popupAnchor: [10,-37]
 })
-let  map = null
-//let clickedMarker = null;
-let stopsLayer = null // contiendra tous les arrêts de bus
+let  map = null // stock de la carte 
+
+let lineLayer = null
+
+let stopsLayer = null // stock du layer 
 
 const initMap = () => {
     console.log('initMap', myPosition)
@@ -43,6 +50,7 @@ const initMap = () => {
     }).addTo(map);
 
     stopsLayer = L.layerGroup().addTo(map)
+    lineLayer = L.layerGroup().addTo(map)
 
     L.control.scale({imperial: false, updateWhenIdle: true}).addTo(map);
 
@@ -51,16 +59,11 @@ const initMap = () => {
         icon: iconPosition
       }
 
-    L.marker([myPosition.latitude, myPosition.longitude], optionsMarker).addTo(map)
+      L.marker([myPosition.latitude, myPosition.longitude], optionsMarker).addTo(map)
     
     
     affStops(map, myPosition, true)
     
-    /*
-    //Philippeville
-    let newPosition = {latitude:50.196241, longitude: 4.543508 }
-    affStops(map,newPosition, true or false)
-    */
 
     map.on('click', function(e) {
         const clickedLatLng = e.latlng;
@@ -84,9 +87,6 @@ const initMap = () => {
     
       
     });
-    
-        
-   
 }
 
 const affStops = (map,position, start=false) => {
@@ -107,23 +107,71 @@ const affStops = (map,position, start=false) => {
         resp.results.forEach(stop => {
             //deconstruction de l'objet
            const {lat, lon} = stop.geo_point_2d
-           const {pot_nom_ha } = stop
+           const {pot_nom_ha, pot_id } = stop
             
-            
-             let marker = L.marker([lat-0.000617, lon+0.0011], {icon:iconStop}).bindPopup(pot_nom_ha)
+            // Créer un marker qui utilise des variables
+            let marker = L.marker([lat-0.000617, lon+0.0011], {icon:iconStop})
+                .on('click',event => {let template = getBus(pot_id, marker, pot_nom_ha)})
+            // Si on est pas au début (start == false), on ajoute le market au layer
              if (!start) {
                 stopsLayer.addLayer(marker)
             } else {
                 marker.addTo(map)
             }
-        })
+
 
     })
+})
     // Si erreur renvoi dans la console et lance une alert
    .catch(err => {
        console.log(err)
        alert('')
    })
+
 }
 
-/* philippevile : 50.196241, 4.543508 */
+const getBus = (pot_id, marker, pot_nom_ha) => {
+    fetch(`https://cepegra-frontend.xyz/bootcamp/stops/${pot_id}`)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp)
+        let template = `
+        <p><strong>${pot_nom_ha}</strong></p>`
+        if(resp.code == "ok" && resp.nbhits > 0) {
+            resp.content.forEach(line => {
+            template += `<p class="marker-link" onclick="affLine('${line.shape_id}');">${line.route_short_name} - ${line.route_long_name}</p>`
+            })
+        } else if(resp.code == "pas ok") {
+            template += `<p> class="eeror">Pas de bus</p>`
+        }
+        marker.bindPopup(template).openPopup()
+
+    })
+    .catch(err => {
+        console.log(err)
+        alert('Erreur serveur')
+    })
+}
+
+affLine = (shape_id) => {
+    console.log('shape_id')
+    lineLayer.clearLayers()
+    fetch(`https://cepegra-frontend.xyz/bootcamp/shapes/${shape_id}`)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp)
+        const dataPositions = resp.content.map(el => [el.shape_pt_lat, el.shape_pt_lon])
+        console.log(dataPositions[0])
+        let marker
+        // Icon de début de course
+        marker = L.marker(dataPositions[0], {icon: iconStart}).bindPopup('Départ')
+        lineLayer.addLayer(marker)
+        // Icon de fin de course 
+        marker = L.marker(dataPositions[dataPositions.length-1], {icon: iconEnd}).bindPopup('Arrivée')
+        lineLayer.addLayer(marker)
+        let polyline = L.polyline(dataPositions, {color:'red', weight : 10})
+        lineLayer.addLayer(polyline)
+    })
+    .catch(err => console.log(err))
+}
+
